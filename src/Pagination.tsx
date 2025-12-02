c08889e6b78f2836a788687cbd3ca5acf6fb539f
osama1644 <latifosama605@gmail.com>
2025-12-02 09:03:57 +0200

    fix pagination

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,34 +1,34 @@
+import React from "react";
 import ReactPaginate from "react-paginate";
 import { useMovies } from "./hooks";
 
 function Pagination() {
   const { fetchData, numOfPages } = useMovies();
   const pageCount = numOfPages;
+
   function handlePageClick(e: { selected: number }) {
-    console.log(e.selected + 1);
     fetchData(e.selected + 1);
   }
+
   return (
-    <>
-      <div className="flex my-5 justify-center">
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel="التالي >"
-          previousLabel="< السابق"
-          pageCount={pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={2}
-          onPageChange={handlePageClick}
-          renderOnZeroPageCount={null}
-          containerClassName="flex gap-2 p-2 bg-[#CBCBCC] rounded"
-          pageClassName="cursor-pointer text-white font-bold  px-1 md:px-3 py-1 rounded transition"
-          activeClassName="bg-[#1E1E1E]"
-          previousClassName="cursor-pointer text-white font-bold bg-[#1e1e1e91] px-1 md:px-3 py-1 rounded transition hover:bg-[#1E1E1E]"
-          nextClassName="cursor-pointer text-white font-bold bg-[#1e1e1e91] px-1 md:px-3 py-1 rounded transition hover:bg-[#1E1E1E]"
-          breakClassName="cursor-default text-white px-2"
-        />
-      </div>
-    </>
+    <div className="flex py-5 justify-center bg-[#1D1D1D]">
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="التالي >"
+        previousLabel="< السابق"
+        pageCount={pageCount}
+        marginPagesDisplayed={0}  
+        pageRangeDisplayed={3}    
+        onPageChange={handlePageClick}
+        renderOnZeroPageCount={null}
+        containerClassName="flex gap-2 p-2 bg-gray-500 rounded"
+        pageClassName="cursor-pointer text-black font-bold px-2 py-1 rounded transition"
+        activeClassName="bg-gray-300"
+        previousClassName="cursor-pointer text-black font-bold bg-gray-200 px-2 py-1 rounded transition hover:bg-gray-300"
+        nextClassName="cursor-pointer text-black font-bold bg-gray-200 px-2 py-1 rounded transition hover:bg-gray-300"
+        breakClassName="cursor-default text-black px-2"
+      />
+    </div>
   );
 }
 
